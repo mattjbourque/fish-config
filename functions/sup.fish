0fc43bdaab3259b904c0a3f2ac602b56ff34efdd
Matthew Bourque <mattjbourque@gmail.com>
2025-05-14 09:45:50 -0500

    New sup fuction for Sakai page updates

---
+++ functions/sup.fish
@@ -0,0 +1,16 @@
+function sup
+
+    set uploads questions up_solutions slides
+    set directories /home/mbourque/Dropbox/Teaching/118math_Su25/Classwork/ /home/mbourque/Dropbox/Teaching/118math_Su25/Slides/
+
+    for dir in $directories
+	rclone --config /home/mbourque/Dropbox/Teaching/Utilities/rclone.conf mkdir 118math_Su25:(basename $dir)
+	for subdir in (find $dir -type d)
+	    for file in $uploads
+		if test -e $subdir/$file.pdf
+		    rclone --quiet --config /home/mbourque/Dropbox/Teaching/Utilities/rclone.conf copyto $subdir/$file.pdf 118math_Su25:(basename $dir)/(basename $subdir)-$file.pdf
+		end
+	    end
+	end
+    end
+end
